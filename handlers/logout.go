12b3df9d99f262c3feda418f7b79bd48f9081260
agungdhewe <agung_dhewe@yahoo.com>
2024-06-20 17:15:20 +0700

    add login

--- handlers/logout.go
+++ handlers/logout.go
@@ -1,22 +1,19 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/fgtago/fgweb/appsmodel"
 	"github.com/fgtago/fgweb/defaulthandlers"
 )
 
-func (hdr *Handler) Home(w http.ResponseWriter, r *http.Request) {
+func (hdr *Handler) Logout(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	pv := ctx.Value(appsmodel.PageVariableKeyName).(*appsmodel.PageVariable)
-	pv.PageName = "home"
+	pv.PageName = "logout"
 	pv.Request = r
 	pv.Response = w
 	pv.Use(hdr.LoginCheck)
-
-	fmt.Println("username:", pv.UserNickName)
 
 	defaulthandlers.SimplePageHandler(pv, w, r)
 }
