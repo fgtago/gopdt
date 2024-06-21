98a9f83fe8ef470d6d7913b86239e03e3419a6d4
agungdhewe <agung_dhewe@yahoo.com>
2024-06-21 15:24:16 +0700

    buat halaman logout

--- handlers/logout.go
+++ handlers/logout.go
@@ -13,5 +13,18 @@ func (hdr *Handler) Logout(w http.ResponseWriter, r *http.Request) {
 	pv.PageName = "logout"
 	pv.Use(hdr.LoginCheck)
 
+	pv.IsAuthenticated = false
+	pv.UserId = ""
+	pv.UserNickName = ""
+	pv.UserFullName = ""
+
+	// logout dari sistem
+	ws := hdr.Webservice
+	ws.Session.Remove(r.Context(), string(appsmodel.IsAuthenticatedKeyName))
+	ws.Session.Remove(r.Context(), string(appsmodel.UserIdKeyName))
+	ws.Session.Remove(r.Context(), string(appsmodel.UserNickNameKeyName))
+	ws.Session.Remove(r.Context(), string(appsmodel.UserFullNameKeyName))
+	ws.Session.RenewToken(r.Context())
+
 	defaulthandlers.SimplePageHandler(pv, w, r)
 }
